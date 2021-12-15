fcfec2929d3da6f6400fa694f4fb7a3531ff14c8
Ruzbellit <ruzbellit.romero@correounivalle.edu.co>
2021-12-15 01:46:51 -0500

    Se actualizo wait_status

--- wait_status.c
+++ wait_status.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <stdbool.h>
 
-int fibonacci(int num) {
-   if ( num == 0 )
-     return 0;
-   else if ( num == 1 )
-     return 1;
-   else
-     return ( fibonacci(num-1) + fibonacci(num-2) );
+int maxrand(int max) {
+  srand(time(0));
+  return rand()%max;
 }
 
 int main(int argc, char *argv[])
@@ -21,14 +19,27 @@ int main(int argc, char *argv[])
         exit(1);
     } else if (rc == 0) {
         // child (new process)
+        int numRand;
+        numRand = maxrand(50);
         printf("[%d] Soy el hijo\n", (int) getpid());
-        printf("el fibonacci de 25 es: %d\n", fibonacci(25));
+        printf("[%d] valor aleatorio hijo %d \n", (int) getpid(), numRand);
+        printf("[%d] Termino ejecucion\n", (int) getpid());
+        return numRand;
     } else {
         // parent goes down this path (original process)
-        wait(NULL);
+        int returnhijo;
         printf("[%d] Soy el padre\n", (int) getpid());
+        wait(&returnhijo);
+        printf("[%d] el valor obtenido con wait(&status) es %d \n", (int) getpid(), returnhijo);
+        printf("[%d] la validacion de WIFEXITED es %d \n", (int) getpid(), WIFEXITED(returnhijo));
+        // el valor retornado por WIFEXITED es true si el hijo termino normalmente
+        if (WIFEXITED(returnhijo) == true){
+          printf("[%d] el valor de salida del proceso hijo es %d \n", (int) getpid(), WEXITSTATUS(returnhijo));
+        }
+        
+        //return 0;
     }
     printf("[%d] Termino ejecucion\n", (int) getpid());
-    return 0;
+    //return 0;
 }
 
